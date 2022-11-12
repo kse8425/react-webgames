68ef911b6c33ff38869369c99b060007240f1771
Kim SungEun <kse8425z@gmail.com>
2022-11-12 13:41:55 +0900

    끝말잇기 hooks 형태로 변경

--- 2. 끝말잇기/WordRelay.jsx
+++ 2. 끝말잇기/WordRelay.jsx
@@ -1,6 +1,43 @@
 const React = require('react');
+const { useState, useRef } = React;
 
-class WordRelay extends React.Component {
+const WordRelay = () => {
+  const [word, setWord] = useState('김성은');
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputRef = useRef(null);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+      setResult('딩동댕');
+      setWord(value);
+      setValue('');
+      inputRef.current.focus();
+    } else {
+      setResult('땡');
+      setValue('');
+      inputRef.current.focus();
+    }
+  };
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputRef} value={value} onChange={onChangeInput} />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
+
+class WordRelay1 extends React.Component {
   state = {
     word: '김성은',
     value: '',
@@ -9,43 +46,9 @@ class WordRelay extends React.Component {
 
   input;
 
-  onSubmitForm = (e) => {
-    e.preventDefault();
-    if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
-      this.setState({
-        result: '딩동댕',
-        word: this.state.value,
-        value: '',
-      });
-      this.input.focus();
-    } else {
-      this.setState({
-        result: '땡',
-        value: '',
-      });
-      this.input.focus();
-    }
-  };
-
-  onChangeInput = (e) => {
-    this.setState({ value: e.target.value });
-  };
-
   onRefInput = (c) => {
     this.input = c;
   };
-  render() {
-    return (
-      <>
-        <div>{this.state.word}</div>
-        <form onSubmit={this.onSubmitForm}>
-          <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
-          <button>입력!</button>
-          <h1>핫로더11</h1>
-        </form>
-        <div>{this.state.result}</div>
-      </>
-    );
-  }
+  render() {}
 }
 module.exports = WordRelay;
